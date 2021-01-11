334698128ba9f007fc30e24d9c52a6d8832c8895
ikramhasib007 <ikramhasib007@gmail.com>
2021-01-11 23:36:35 +0600

    posts fragment added

--- graphql-prisma/src/resolvers/User.js
+++ graphql-prisma/src/resolvers/User.js
@@ -12,6 +12,19 @@ const User = {
         return null
       }
     }
+  },
+  posts: {
+    fragment: 'fragment userId on User { id }',
+    resolve(parent, args, { prisma }, info) {
+      return prisma.query.posts({
+        where: {
+          published: true,
+          author: {
+            id: parent.id
+          }
+        }
+      })
+    }
   }
 }
 
