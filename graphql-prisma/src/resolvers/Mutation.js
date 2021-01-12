e4baabd738b4a3d8c6c88b9c89cb5d069b37b98c
ikramhasib007 <ikramhasib007@gmail.com>
2021-01-12 21:39:55 +0600

    password updated

--- graphql-prisma/src/resolvers/Mutation.js
+++ graphql-prisma/src/resolvers/Mutation.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcryptjs'
 import getUserId from '../utils/getUserId'
 import generateToken from '../utils/generateToken'
+import hashPassword from '../utils/hashPassword'
 
 const Mutation = {
   async createUser(parent, args, { prisma }, info) {
@@ -9,10 +10,7 @@ const Mutation = {
     // if(emailTaken) {
     //   throw new Error('Email taken')
     // }
-    if(args.data.password.length < 8) {
-      throw new Error('Password must be 8 characters or longer.')
-    }
-    const password = await bcrypt.hash(args.data.password, 10);
+    const password = await hashPassword(args.data.password)
     const user = await prisma.mutation.createUser({data: {...args.data, password}});
     return {
       user, token: generateToken(user.id)
@@ -43,7 +41,9 @@ const Mutation = {
   },
   async updateUser(parent, args, { prisma, request }, info) {
     const userId = getUserId(request);
-
+    if(typeof args.data.password === 'string') {
+      args.data.password = hashPassword(args.data.password)
+    }
     return prisma.mutation.updateUser({
       where: {
         id: userId
